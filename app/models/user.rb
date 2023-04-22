b0a56ac06b8f6529722504b61ede9c981ca0faa8
nakanotomamu <kasaokanisi10@gmail.com>
2023-04-22 02:20:21 +0000

    second commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email,presence: true
-  validates :password,length:{minimum: 8,maximum: 23}
-  validates :password, format: { with: /\A(?=.[a-z])(?=.[@])(?=.*?\d)[a-z\d]{0,15}+\z/i }
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email,presence: true, format: { with: /[a-zA-Z0-9._+]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*/ }
+  # validates :password,length:{minimum: 8,maximum: 23}
+  # validates :password, format: { with: /\A(?=.[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  # validates :email,format: { with: /\A[~ @ ~ . ~]}
+  PW_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i
+  validates :password, presence: true, format: { with: PW_REGEX }
   has_secure_password
 end
-
- 
