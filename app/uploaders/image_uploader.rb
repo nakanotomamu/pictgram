3615c51b29459096ef3852960f5b89e1d28ca092
nakanotomamu <kasaokanisi10@gmail.com>
2023-04-29 04:09:58 +0000

    seven commit

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -11,8 +11,17 @@ class ImageUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    
   end
-
+  
+  def extension_whitelist
+   %w(jpg jpeg gif png)
+  end
+  
+   def size_range
+    0..10.megabytes
+   end
+    
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
