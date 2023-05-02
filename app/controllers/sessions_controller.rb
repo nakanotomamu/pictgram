2c6f668b10cfc8866b04bdf54ef2a6a9d401b85a
nakanotomamu <kasaokanisi10@gmail.com>
2023-05-02 14:29:53 +0000

    eleven commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: session_params[:email])
-    if user && user.authenticate(params[:session][:password])
+    # binding.pry
+    if user && user.authenticate(session_params[:password])
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
     else
@@ -21,7 +22,7 @@ class SessionsController < ApplicationController
   
    private
   def session_params
-    params.require(:session).permit(:email, :passworad)
+    params.require(:session).permit(:email, :password)
   end
 
   def log_in(user)
